e8a4f1c24b52a851e1cc2f3e9821dc76bcf569bc
Marnix van den Bent <marnix.van.den.bent@rabobank.nl>
2020-01-09 17:12:37 +0100

    Updated copyright year

--- test/unit/vc/vc-getters.test.ts
+++ test/unit/vc/vc-getters.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Coöperatieve Rabobank U.A.
+ * Copyright 2020 Coöperatieve Rabobank U.A.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
